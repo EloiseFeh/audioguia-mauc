014e49167faa9d0e7a380c766c3f9aa755bb8a97
Eloise Ferreira <eloiseferreira045@gmail.com>
2021-07-29 01:44:46 -0300

    audio style

--- src/pages/audio.js
+++ src/pages/audio.js
@@ -1,36 +1,35 @@
-import React from 'react';
-import AudioPlayer from 'react-h5-audio-player';
-import 'react-h5-audio-player/lib/styles.css';
-import 'react-h5-audio-player/lib/styles.less';
-import { Link, useLocation } from 'react-router-dom';
+import "../styles/audio.css";
+import "../styles/global.css";
+import React from "react";
+import AudioPlayer from "react-h5-audio-player";
+import "react-h5-audio-player/lib/styles.css";
+import "react-h5-audio-player/lib/styles.less";
+import { Link, useLocation } from "react-router-dom";
 
 export default function Player() {
   const { state } = useLocation();
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>{ state.item.nome }</h1>
+    <div className="container">
+      <div className="audioContainer">
+        <div className="coverContainer"></div>
+
+        <h1>{state.item.nome}</h1>
         <AudioPlayer
-    src={ process.env.PUBLIC_URL + '/audios/' + `${state.item.key}` + '.mp3' }
-    onPlay={e => console.log("onPlay")}
-  />
-        <Link
-          className="App-link"
-          to="/"
-        >
-          Voltar
-        </Link>
-        <a
-          className="App-link"
-          href="https://mauc.ufc.br/pt/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Site do Mauc
-        </a>
-      </header>
+          className="audioPlayer"
+          src={
+            process.env.PUBLIC_URL + "/audios/" + `${state.item.key}` + ".mp3"
+          }
+          onPlay={(e) => console.log("onPlay")}
+        />
+      </div>
+      
+      <div className="audioInfo">
+          <h3>Nome da Obra</h3>
+          <h3>Nome do autor</h3>
+          <p>O idioma da descrição deve mudar de acordo com a linguagem escolhida</p>
+      </div>
+
     </div>
   );
 }
-
